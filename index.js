480061362333f9d22d48e9ae8a3d8f82454ff5d8
Soledad Noelia Gomez <95485411+soledadngomez@users.noreply.github.com>
2022-09-12 21:03:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let saludo = () => {
 };
 
 pedirEdades = () => {
-	for (let i = 0; i <= 3; i++) {
+	for (let i = 0; i <= 5; i++) {
 		arrayEdades[i] = parseInt(prompt(`Dame la edad ${i + 1}`));
 	}
 };
